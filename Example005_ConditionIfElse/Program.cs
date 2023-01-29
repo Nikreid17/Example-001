092233cc5a3bec78823282707e7423ca5f243347
Nickolay <nik5te@yandex.ru>
2023-01-29 19:21:36 +0300

    Example 005 If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+ 
+ if (username.ToLower() == "николай")
+ {Console.Write("Здоровеньки булы, ");
+ Console.WriteLine(username);}
+ else 
+ {Console.Write("Привет, ");
+ Console.WriteLine(username);}
+
